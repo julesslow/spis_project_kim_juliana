f13026ac068dfda76c3d0204dba194efaec2b47b
julesslow <j3low@ucsd.edu>
2025-08-29 17:27:38 +0000

    made stuff
    Co-authored-by: kimmwie <kimmwie@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
-from flask import *
-from aubio import source, onset
+#from flask import *
+import sys, os
+#from aubio import source, onset
+#import librosa
+import spleeter
+# def get_onset_times(file_path):
+#     window_size = 1024 # FFT size
+#     hop_size = window_size // 4
 
-def get_onset_times(file_path):
-    window_size = 1024 # FFT size
-    hop_size = window_size // 4
-
-    sample_rate = 0
-    src_func = source(file_path, sample_rate, hop_size)
-    sample_rate = src_func.samplerate
-    onset_func = onset('default', window_size, hop_size)
+#     sample_rate = 0
+#     src_func = source(file_path, sample_rate, hop_size)
+#     sample_rate = src_func.samplerate
+#     onset_func = onset('default', window_size, hop_size)
     
-    duration = float(src_func.duration) / src_func.samplerate
+#     duration = float(src_func.duration) / src_func.samplerate
 
-    onset_times = [] # seconds
-    while True: # read frames
-        samples, num_frames_read = src_func()
-        if onset_func(samples):
-            onset_time = onset_func.get_last_s()
-            if onset_time < duration:
-                onset_times.append(onset_time)
-            else:
-                break
-        if num_frames_read < hop_size:
-            break
+#     onset_times = [] # seconds
+#     while True: # read frames
+#         samples, num_frames_read = src_func()
+#         if onset_func(samples):
+#             onset_time = onset_func.get_last_s()
+#             if onset_time < duration:
+#                 onset_times.append(onset_time)
+#             else:
+#                 break
+#         if num_frames_read < hop_size:
+#             break
     
-    return onset_times
+#     return onset_times
+
+def main(args):
+    filename = args[0]
+
+    os.system(f"""
+        docker run --rm \
+              - v $(pwd)/input:/input \
+              -v $(pwd)/outputs:/output \
+              -v $MODEL_DIRECTORY:/model \
+              -e MODEL_PATH=/model \
+              deezer/speeter:3.8-4stems \
+              separate -o /output -p spleeter:4stems /input/{filename}
+    """)
+
+if __name__=="__main__":
+    main(sys.argv[1:])
