5a035168833b97d031a36990cedaf34f5ecc7e1e
julesslow <j3low@ucsd.edu>
2025-09-01 18:15:36 +0000

    made generate_beatmap.py and song_split.py work

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
-#from flask import *
-import sys, os
-#from aubio import source, onset
-#import librosa
-import spleeter
-# def get_onset_times(file_path):
-#     window_size = 1024 # FFT size
-#     hop_size = window_size // 4
+import librosa
+import generate_beatmap
 
-#     sample_rate = 0
-#     src_func = source(file_path, sample_rate, hop_size)
-#     sample_rate = src_func.samplerate
-#     onset_func = onset('default', window_size, hop_size)
-    
-#     duration = float(src_func.duration) / src_func.samplerate
-
-#     onset_times = [] # seconds
-#     while True: # read frames
-#         samples, num_frames_read = src_func()
-#         if onset_func(samples):
-#             onset_time = onset_func.get_last_s()
-#             if onset_time < duration:
-#                 onset_times.append(onset_time)
-#             else:
-#                 break
-#         if num_frames_read < hop_size:
-#             break
-    
-#     return onset_times
-
-def main(args):
-    filename = args[0]
-
-    os.system(f"""
-        docker run --rm \
-              - v $(pwd)/input:/input \
-              -v $(pwd)/outputs:/output \
-              -v $MODEL_DIRECTORY:/model \
-              -e MODEL_PATH=/model \
-              deezer/speeter:3.8-4stems \
-              separate -o /output -p spleeter:4stems /input/{filename}
-    """)
-
-if __name__=="__main__":
-    main(sys.argv[1:])
+def main():
+    file_path = '../game/audio/my-music.ogg'
+    x, sr = librosa.load(file_path)
+    onset_frames = librosa.onset.onset_detect(x, sr=sr, wait=1, pre_avg=1, post_avg=1, pre_max=1, post_max=1)
+    onset_times = librosa.frames_to_time(onset_frames)
+    # remove extension, .mp3, .wav etc.
+    file_name_no_extension, _ = os.path.splitext(file_path)
+    output_name = file_name_no_extension + '.beatmap.txt'
+    with open(output_name, 'wt') as f:
+        f.write('\n'.join(['%.4f' % onset_time for onset_time in onset_times]))
