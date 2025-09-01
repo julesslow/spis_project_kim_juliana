375862d59f9f172bf4ec86f956d7c3b08d13bbb2
kimmwie <kimantoinettebarcelos@gmail.com>
2025-09-01 18:15:28 +0000

    added background

---
+++ script.js
@@ -0,0 +1,20 @@
+// vars
+
+// funcs (setup)
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(1440, 900);
+  background(255, 247, 252)
+}
+
+function draw() {
+
+}
+
+// funcs (screens)
+function startingScreen(){
+  
+}
